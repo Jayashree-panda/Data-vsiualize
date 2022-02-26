075544bee6b75c59347807a4d08db06803e4501a
jayap12 <jayashree.panda@geeksforgeeks.org>
2022-02-26 17:12:58 +0530

    design UI

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="data-wrapper">
+        <div className="child-element">
+          <p>Mean</p>
+          <p>123456</p>
+        </div>
+        <div className="child-element">
+          <p>Median</p>
+          <p>123456</p>
+        </div>
+        <div className="child-element">
+          <p>Std Deviation</p>
+          <p>123456</p>
+        </div>
+        <div className="child-element">
+          <p>Mode</p>
+          <p>123456</p>
+        </div>
+      </div>
+      <div className="customize-data-container">
+        <div className="input-container">
+          <input placeholder="Enter a number" className="number-input"/>
+          <button className="submit-button">Submit</button>
+        </div>
+        <div className="data-buttons-container">
+          <button className="reload-button">Reload JSON-1234 Data</button>
+          <button className="reload-button">Reload JSON-4321 Data</button>
+        </div>
+      </div>
     </div>
   );
 }
